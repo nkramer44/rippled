9702b50aac7496c8341414258c7f1dee917f4c30
Mayukha Vadari <mvadari@ripple.com>
2022-11-03 14:11:07 -0400

    pass account str to Python

--- src/ripple/app/tx/impl/DummyTx.cpp
+++ src/ripple/app/tx/impl/DummyTx.cpp
@@ -26,8 +26,51 @@ DummyTx::preflight(PreflightContext const& ctx)
     if (ctx.tx.getFlags() & tfUniversalMask)
         return temINVALID_FLAG;
     
+    // initialize use of Python
     Py_Initialize();
-    PyRun_SimpleString("print('*' * 2000)");
+    PyRun_SimpleString("import sys");
+    PyRun_SimpleString("import os");
+
+    // TODO: figure out how to replace this hardcoded path with `__FILE__`
+    PyRun_SimpleString("sys.path.append(\"../src/ripple/app/tx/impl\")");
+
+    PyRun_SimpleString("print(sys.path)");
+    PyRun_SimpleString("print(os.getcwd())");
+
+    PyObject *pName, *pModule, *pFunc, *pArgs, *pValue;
+
+    // import the Python file
+    pName = PyUnicode_DecodeFSDefault("dummy_tx");
+    pModule = PyImport_Import(pName);
+    Py_DECREF(pName);
+    if (pModule == NULL) {
+        return temUNKNOWN;
+    }
+
+    PyRun_SimpleString("print('IMPORT SUCCESSFUL')");
+
+    // get the name of the function
+    pFunc = PyObject_GetAttrString(pModule, "preflight");
+
+    if ( !( pFunc && PyCallable_Check(pFunc) ) ) {
+        if (PyErr_Occurred())
+            PyErr_Print();
+        PyRun_SimpleString("print('Cannot find function preflight')");
+        return temUNKNOWN;
+    }
+
+    PyRun_SimpleString("print('FUNCTION RETRIEVAL SUCCESSFUL')");
+
+    auto const account = ctx.tx[sfAccount];
+
+    pArgs = PyTuple_New(1);
+
+    pValue = PyUnicode_FromString(toBase58(account).c_str());
+    PyTuple_SetItem(pArgs, 0, pValue);
+
+    PyObject_CallObject(pFunc, pArgs);
+
+    PyRun_SimpleString("print('FUNCTION CALL SUCCESSFUL')");
 
     if (Py_FinalizeEx() < 0) {
         return temUNKNOWN;
---
+++ src/ripple/app/tx/impl/dummy_tx.py
@@ -0,0 +1,3 @@
+def preflight(account = None):
+    print('p' * 2000)
+    print(account)
