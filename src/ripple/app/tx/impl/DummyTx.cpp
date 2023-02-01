3fd5ae09ed65cc3ddaaabec3db99b3260ee27d45
Mayukha Vadari <mvadari@ripple.com>
2023-02-01 14:58:16 -0500

    get pybind working

--- src/ripple/app/tx/impl/DummyTx.cpp
+++ src/ripple/app/tx/impl/DummyTx.cpp
@@ -9,8 +9,9 @@
 #include <ripple/protocol/TxFlags.h>
 #include <ripple/protocol/st.h>
 
-#include <Python.h>
-
+#include <pybind11/embed.h> // everything needed for embedding
+namespace py = pybind11;
+using namespace pybind11::literals; // to bring in the `_a` literal
 
 namespace ripple {
 
@@ -23,58 +24,15 @@ DummyTx::preflight(PreflightContext const& ctx)
     if (ctx.tx.getFlags() & tfUniversalMask)
         return temINVALID_FLAG;
     
-    // initialize use of Python
-    Py_Initialize();
-    PyRun_SimpleString("import sys");
-    PyRun_SimpleString("import os");
+    py::scoped_interpreter guard{}; // start the interpreter and keep it alive
 
-    // TODO: figure out how to replace this hardcoded path with `__FILE__`
-    PyRun_SimpleString("sys.path.append(\"../src/ripple/app/tx/impl\")");
-
-    PyRun_SimpleString("print(sys.path)");
-    PyRun_SimpleString("print(os.getcwd())");
-
-    PyObject *pName, *pModule, *pFunc, *pArgs, *pValue;
-
-    // import the Python file
-    pName = PyUnicode_DecodeFSDefault("dummy_tx");
-    pModule = PyImport_Import(pName);
-    Py_DECREF(pName);
-    if (pModule == NULL) {
-        return temUNKNOWN;
-    }
-
-    // std::string currentFilename = __FILE__;
-    // PyRun_SimpleString("print('" + currentFilename + "')");
-
-    PyRun_SimpleString("print('IMPORT SUCCESSFUL')");
-
-    // get the name of the function
-    pFunc = PyObject_GetAttrString(pModule, "preflight");
-
-    if ( !( pFunc && PyCallable_Check(pFunc) ) ) {
-        if (PyErr_Occurred())
-            PyErr_Print();
-        PyRun_SimpleString("print('Cannot find function preflight')");
-        return temUNKNOWN;
-    }
-
-    PyRun_SimpleString("print('FUNCTION RETRIEVAL SUCCESSFUL')");
-
-    auto const account = ctx.tx[sfAccount];
-
-    pArgs = PyTuple_New(1);
-
-    pValue = PyUnicode_FromString(toBase58(account).c_str());
-    PyTuple_SetItem(pArgs, 0, pValue);
-
-    PyObject_CallObject(pFunc, pArgs);
-
-    PyRun_SimpleString("print('FUNCTION CALL SUCCESSFUL')");
-
-    if (Py_FinalizeEx() < 0) {
-        return temUNKNOWN;
-    }
+    py::print("Hello, World!"); // use the Python API
+    py::object plugin = py::module_::import("plugin");
+    py::print(plugin);
+    py::object preflight = plugin.attr("preflight");
+    py::print(preflight);
+    py::print(ctx.tx);
+    preflight(ctx.tx);
 
     return preflight2(ctx);
 }
