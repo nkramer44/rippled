45daf561a0c5910854d80ea53ea2e33ca5472b49
Mayukha Vadari <mvadari@ripple.com>
2023-02-03 10:43:44 -0500

    update for doApply work

--- src/ripple/app/tx/impl/Transactor.h
+++ src/ripple/app/tx/impl/Transactor.h
@@ -109,12 +109,6 @@ public:
         return ctx_.view();
     }
 
-    ApplyView const&
-    view() const
-    {
-        return ctx_.view();
-    }
-
     /////////////////////////////////////////////////////
     /*
     These static functions are called from invoke_preclaim<Tx>
